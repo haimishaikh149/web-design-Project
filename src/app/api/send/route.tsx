b80cc7fda77f20d52b8b016d50689293d294b511
Hira shaikh <160590123+haimishaikh149@users.noreply.github.com>
2024-11-15 01:07:40 +0500

    Update route.tsx

--- src/app/api/send/route.tsx
+++ src/app/api/send/route.tsx
@@ -1,8 +1,9 @@
 
 import { NextResponse } from 'next/server';
-import { Resend } from 'resend';  // sirf yeh wala import rakhein
 
-const resend = new Resend(process.env.RESEND_API_KEY || '');
+import Resend from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
   try {
