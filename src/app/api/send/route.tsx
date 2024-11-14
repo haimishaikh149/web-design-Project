88dbb6c9ac65d25021bd775371da891058adb4ae
Hira shaikh <160590123+haimishaikh149@users.noreply.github.com>
2024-11-15 00:39:14 +0500

    Update route.tsx

--- src/app/api/send/route.tsx
+++ src/app/api/send/route.tsx
@@ -1,46 +1,8 @@
 
-// import resend from 'resend';
-
-// import { NextResponse } from 'next/server';
-// // import { EmailTemplate} from '../../../components/EmailTemplate';
-// import { Resend } from 'resend';
-
-
-// const resend = new Resend(process.env.RESEND_API_KEY);
-
-// export async function POST() {
-//   try {
-//     const { data, error } = await resend.emails.send({
-//       from: 'Acme <haimishaikh149@gmail.com>',
-//       to: ['haimishaikh149@gmail.com'],
-//       subject: 'Hello world',
-//       react:( 
-//       <>
-//       <p>Email Body</p>
-      
-//       </>
-      
-//       )
-//     });
-
-//     if (error) {
-//       return NextResponse.json({ error }, { status: 500 });
-//     }
-
-//     return NextResponse.json(data);
-//   } catch (error) {
-//     return Response.json({ error }, { status: 500 });
-//   }
-// }
-
-
-
-
-
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';  // sirf yeh wala import rakhein
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY || '');
 
 export async function POST() {
   try {
