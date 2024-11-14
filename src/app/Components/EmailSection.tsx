7ad7a52437c2adb8d830b59a77b639c8298e2538
Hira shaikh <160590123+haimishaikh149@users.noreply.github.com>
2024-11-14 23:35:01 +0500

    Update EmailSection.tsx

--- src/app/Components/EmailSection.tsx
+++ src/app/Components/EmailSection.tsx
@@ -33,9 +33,6 @@ const EmailSection = () => {
     <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
       <div>
         <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
-        {/* <p className="text-[#ADB7BE] md-4 max-w-md">
-          I'm currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you.
-        </p> */}
 
 
 <p className="text-[#ADB7BE] md-4 max-w-md">
